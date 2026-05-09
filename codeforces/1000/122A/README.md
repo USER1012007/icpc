8c96ac0e29af19badaeafe1cbb7090e391281261
User1012007 <rojasbadilloe@gmail.com>
2026-05-09 15:48:33 -0600

    122A: add solution to problem 122A ez

---
+++ codeforces/1000/122A/README.md
@@ -0,0 +1,15 @@
+# Problema
+[122A](https://codeforces.com/problemset/problem/122/A)
+
+# Dificultad
+1000
+
+# Tags
+brute force
+number theory 
+
+# Solución
+como son pocos numeros del 1 al 1000 que tienen 4 y 7, pues un if alv de tds si son multiplos
+
+# Autor
+emilio
---
+++ codeforces/1000/122A/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+
+  int n;
+  cin >> n;
+  if (n % 4 == 0 || n % 7 == 0 || n % 44 == 0 || n % 47 == 0 || n % 74 == 0 ||
+      n % 77 == 0 || n % 444 == 0 || n % 447 == 0 || n % 474 == 0 ||
+      n % 477 == 0 || n % 744 == 0 || n % 747 == 0 || n % 774 == 0 ||
+      n % 777 == 0) {
+    cout << "YES";
+  } else {
+    cout << "NO";
+  }
+}
+
+int main(int argc, char *argv[]) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  solve();
+
+  return 0;
+}

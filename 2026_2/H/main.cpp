f9709bf70b6864714290b79fe5a3dbde5b5d292a
User1012007 <rojasbadilloe@gmail.com>
2026-05-17 11:09:09 -0600

    remove bin

--- 2026_2/H/main.cpp
+++ 2026_2/H/main.cpp
@@ -8,14 +8,54 @@ void fast_io() {
   cin.tie(NULL);
 }
 
-void solve() {}
+void solve() {
+  ll n;
+  cin >> n;
+  ll r[n];
+  ll k[n];
+
+  for (ll i = 0; i < n; i++) {
+    cin >> r[i];
+  }
+  for (ll i = 0; i < n; i++) {
+    cin >> k[i];
+  }
+
+  ll T = 0;
+  ll t_men = r[0] + k[0];
+  ll count_men = 1;
+  ll to = 0;
+
+  for (ll i = 1; i < n; i++) {
+    ll t = r[i] + k[i];
+
+    if (r[i] > r[i - 1] && count_men > 0) {
+      if (to + t_men > T)
+        T = to + t_men;
+      count_men = 0;
+      t_men = 0;
+    }
+
+    if (r[i] > r[i - 1]) {
+      to = T;
+      T += t;
+    } else {
+      int tmp = t + count_men;
+      if (tmp > t_men) {
+        t_men = tmp;
+      }
+      count_men++;
+    }
+    if ((i == n - 1) && (to + t_men > T)) {
+      T = to + t_men;
+    }
+  }
+
+  cout << T;
+}
 
 int main() {
   fast_io();
-  int t = 1;
-  // cin >> t;
-  while (t--) {
-    solve();
-  }
+  solve();
   return 0;
 }
